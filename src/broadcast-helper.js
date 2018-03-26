0e90523156d3e95e4eb99479021925e99e5df941
Matheus Rufca <matheusrufca@gmail.com>
2018-03-26 14:53:32 -0300

    Fixed module creator

--- src/broadcast-helper.js
+++ src/broadcast-helper.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	angular
-		.module('ptt.utils.broadcast-helper')
+		.module('ptt.utils.broadcast-helper', [])
 		.factory('BroadcastHelper', ['$rootScope', BroadcastHelper]);
 
 	function BroadcastHelper($rootScope) {
